33702c9825937b67ea86b0360519ae91a98ce880
robsquaadd <102495137+robsquaadd@users.noreply.github.com>
2022-05-05 00:02:01 -0400

    validation using regex added. validates input.

--- Assets/script.js
+++ Assets/script.js
@@ -2,6 +2,8 @@
 var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
 
+//prompts the user for the number of characters, and if the
+//user wants lowercase, uppercase, numbers, and special characters in their password.
 function getRequirements() {
   var numberOfCharacters = prompt(
     "How many characters do you want your password to be? Your password must be between 8 and 128 characters long."
@@ -28,17 +30,23 @@ function getRequirements() {
       },
     ];
     var characterList = [];
+    var confirm = [];
     for (var i = 0; i < promptArray.length; i++) {
-      var confirm = window.confirm(promptArray[i].question);
-      if (confirm) {
+      confirm[i] = window.confirm(promptArray[i].question);
+      if (confirm[i]) {
         characterList = characterList + promptArray[i].string;
       }
     }
-    var requirementsArray = [numberOfCharacters, characterList];
+    if (confirm === []) {
+      alert("Please choose at least one character type.");
+      getRequirements();
+    }
+    var requirementsArray = [numberOfCharacters, characterList, confirm];
     return requirementsArray;
   }
 }
 
+//randomly generates the password
 function generatePassword(array) {
   var passwordArray = [];
   var positionArray = [];
@@ -53,13 +61,38 @@ function generatePassword(array) {
   return passwordString;
 }
 
+function validatePassword(string, array) {
+  //use regular expressions to validate password.
+  //pass in the answer choices by making the confirm variable an array.
+  //returns true or false depending on if the password is a valid password.
+  var regexArray = [
+    /[a-z]+/,
+    /[A-Z]+/,
+    /[0-9]+/,
+    /[ !\"#$%&'()*+,-./:;<=>?@\[\\\]^_`{|}~]/,
+  ];
+  for (i = 0; i < array.length; i++) {
+    if (array[i] === true) {
+      var validationCheck = regexArray[i].test(string);
+      if (validationCheck === false) {
+        console.log(string + " is an invalid password");
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
 //Write password to the #password input
 function writePassword() {
   var characterArray = getRequirements();
   if (characterArray != "") {
-    var password = generatePassword(characterArray);
+    var validPassword = "";
+    while (validPassword === false || validPassword === "") {
+      var password = generatePassword(characterArray);
+      validPassword = validatePassword(password, characterArray[2]);
+    }
     var passwordText = document.querySelector("#password");
-    alert(password);
     passwordText.textContent = password;
   }
 }
